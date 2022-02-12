36056f4050f05698daa7fc266da70bb0b027a127
Caleb Highsmith <calebhighsmith@gmail.com>
2022-02-12 15:54:07 -0600

    Update addToDo Component
    Added an error message so users cannot add empty content to the list using ChakraUI Toast.

--- src/components/addtodo.js
+++ src/components/addtodo.js
@@ -1,14 +1,26 @@
-import { HStack, Input, Button } from '@chakra-ui/react';
+import { HStack, Input, Button, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import {nanoid} from 'nanoid';
 
 
 function AddToDo({addToDo}){
+
+    const toast = useToast();
+
     function submit(e){
         e.preventDefault();
-        console.log(content)
+        if(!content){
+            toast({
+                title: 'No content',
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+                })
+            return
+        }
         const todo = {id: nanoid(), body: content,}
         addToDo(todo)
+        setContent('')
     }
 
     const [content, setContent] = useState('') 
