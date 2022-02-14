58c4f74399a83d9440530898b46ca0350fbbea48
Caleb Highsmith <calebhighsmith@gmail.com>
2022-02-13 20:33:19 -0600

    Dark mode
    Added dark mode using chakraUi. Also added an icon switch using a React onClick hook, when toggled the icon will change from a light bulb that is on to one that is off.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {Heading, Icon} from '@chakra-ui/react';
+import {Heading} from '@chakra-ui/react';
 import TodoList  from './components/Todolist'
 import AddToDo from './components/addtodo'
-import { Stack, HStack, VStack, IconButton} from '@chakra-ui/react'
-import {BsLightbulbFill} from 'react-icons/bs'
+import { useColorMode, VStack, IconButton} from '@chakra-ui/react'
+import {RiLightbulbFlashFill} from 'react-icons/ri'
+import {HiOutlineLightBulb} from 'react-icons/hi'
 import {useState, useEffect} from 'react'
 
 function App() {
@@ -25,9 +26,11 @@ function App() {
     setTodos([...todos, todo]);
   }
 
+  const {colorMode, toggleColorMode} = useColorMode()
+
   return (
     <VStack p={4}>
-    <IconButton icon={<BsLightbulbFill/>} isRound='true' size='lg' alignSelf="flex-end" />
+    <IconButton icon={colorMode === 'light' ? <RiLightbulbFlashFill/> : <HiOutlineLightBulb/>} isRound='true' size='lg' alignSelf="flex-end" onClick={toggleColorMode} />
       <Heading mb="8" fontWeight="extrabold" size="2xl" bgGradient='linear(to-r, red.500, red.300, orange.500)' bgClip="text">Todo Application</Heading>
         <TodoList p={4} todos={todos} deleteTodo={deleteTodo}/>
         <AddToDo addToDo={addToDo} />
